86d48a6c8dfdaac4e0d37c755fc33e948fcefaf4
RapDoodle <bowenbhwu@gmail.com>
2021-05-21 15:34:33 +0800

    Template update

--- init.m
+++ init.m
@@ -1,5 +1,5 @@
 % Add LightNeuNet-MATLAB to path
-if ~isdir('include')
+if ~isfolder('include')
     disp("The LightNeuNet-MATLAB library is not found.");
     disp("Run update.m to download or update the library");
     return;
